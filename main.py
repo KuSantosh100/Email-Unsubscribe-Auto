d2d55c8acf7f34c564c638f987fed2f539e71873
KuSantosh100 <kumarsantos1m1@gmail.com>
2024-11-16 02:36:52 +0530

    Clicking links and extracting the content codes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import imaplib
 from imaplib import IMAP4_SSL
 import email
+from bs4 import BeautifulSoup # pip install beautifulsoup4
+import requests # pip install requests
 
 load_dotenv()
 
@@ -18,25 +20,47 @@ def conn_to_email():
     conn.select("inbox")
     return conn
 
+
+def extract_links(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = [link["href"] for link in soup.find_all("a", href=True) if "unsubscribe" in link["href"].lower()]
+    return links
+
+def click_link(link):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            print("Successfully visited", link)
+        else:
+            print("Failed to visit", link, "error code:", response.status_code)
+    except Exception as e:
+        print("Error with", link, str(e))
+            
+
 def search_for_email():
     conn = conn_to_email()
     _, data = conn.search(None, '(BODY "unsubscribed")')
     data = data[0].split()
+    links = []
+    
     for num in data:
         _, data = conn.fetch(num, '(RFC822)')
         msg = email.message_from_bytes(data[0][1])
         
         if msg.is_multipart():
             for part in msg.walk():
-                if part.get_content_type() == 'text/plain':
+                if part.get_content_type() == 'text/html':
                     html_content = part.get_payload(decode=True).decode()
-                    print(html_content)
+                    links.extend(extract_links_from_html(html_content))
         else:
             content_type = msg.get_content_type()
             content = msg.get_payload(decode=True).decode()
             
             if content_type == 'text/html':
-                print(content)
+                links.extend(extract_links_from_html(content))
     conn.logout()
+    return links
                     
-search_for_email()
+links = search_for_email()
+for link in links:
+    click_link(link)
